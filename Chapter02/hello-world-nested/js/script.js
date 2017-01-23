138e92a2188d9b6268fd74aa0add9271f10ef5fc
xeeshan <xeeshan@gmail.com>
2017-01-23 13:17:10 -0800

    nested Element Create using  extend class React.component

---
+++ Chapter02/hello-world-nested/js/script.js
@@ -0,0 +1,9 @@
+
+let h1 = React.createElement('h1', null, 'Hello World!');
+let p  = React.createElement('h1', null, 'How Are you?');
+
+ReactDOM.render(
+    React.createElement('div', null, h1 ,h1 ,h1 ,p),
+    document.getElementById('content')
+    
+)
