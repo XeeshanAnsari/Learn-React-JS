0e9b98e908ffaa97ea2ec002a46a187d36f3d5e6
xeeshan <xeeshan@gmail.com>
2017-01-23 19:19:12 -0800

    Element property using this.props

--- Chapter02/hello-world/js/script.js
+++ Chapter02/hello-world/js/script.js
@@ -1,9 +1,31 @@
 
-let h1 = React.createElement('h1', null, 'Hello World!');
-let p  = React.createElement('h1', null, 'How Are you?');
+class HelloWorld extends React.Component{
+    render(){
+        return React.createElement(
+          'h1',
+          this.props ,
+          'hello ' + this.props.frameworkName + ' World!!!'
+         );
+    }
+} 
+
 
 ReactDOM.render(
-    React.createElement('div', null, h1 ,h1 ,h1 ,p),
-    document.getElementById('content')
-    
+  React.createElement(
+    'div',
+    null,
+    React.createElement(HelloWorld, {
+      id: 'ember',
+      frameworkName: 'Ember.js',
+      title: 'A framework for creating ambitious web applications.'}),
+    React.createElement(HelloWorld, {
+      id: 'backbone',
+      frameworkName: 'Backbone.js',
+      title: 'Backbone.js gives structure to web applications...'}),
+    React.createElement(HelloWorld, {
+      id: 'angular',
+      frameworkName: 'Angular.js',
+      title: 'Superheroic JavaScript MVW Framework'})
+  ),
+  document.getElementById('content')
 )
