81f4b9b6616e9a4ceee66b3ef687d59edd6b7efe
xeeshan <xeeshan@gmail.com>
2017-01-24 23:37:32 -0800

    digital clock with css style and using state for continue working clock every second

---
+++ chapter04/clock/js/clock.js
@@ -0,0 +1,23 @@
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.launchClock();
+        this.state = { currentTime: new Date().toLocaleString() };
+    }
+    launchClock() {
+        setInterval(() => {
+            console.log('Updating Time ... ');
+            this.setState({
+                currentTime: new Date().toLocaleString()
+            });
+        }, 1000);
+    }
+    render() {
+        console.log('Rendering Time');
+        return React.createElement(
+            'div',
+            { style: { color: 'blue', fontSize: '2em' } },
+            this.state.currentTime
+        );
+    }
+}
